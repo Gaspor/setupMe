d4ee3ff324c30cae0f123526726293c2eb1eacb3
Lucas Filgueiras <lucks_fil@hotmail.com>
2021-09-30 21:53:59 -0300

    conexao front com back

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,32 @@
 let counter = 1;
 
-setInterval(function(){
+setInterval(function () {
     document.getElementById(`radio` + counter).checked = true;
     counter++;
-    if (counter > 4){
+    if (counter > 4) {
         counter = 1;
     }
 }, 5000);
+
+async function getContent() {
+    try {
+        let url = "https://setupme.herokuapp.com/placas_mae";
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            mode: 'no-cors'
+        });
+        
+        const json1 = await response.text();
+        console.log(json1);
+        return `<p>${json1} asdsadsa</p>`;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getContent();
