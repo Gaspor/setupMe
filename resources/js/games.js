0d68a33c26f35cf4180d641b0cae08b9a65f77b5
Lucas Filgueiras <lucks_fil@hotmail.com>
2021-10-19 16:43:59 -0300

    montagem dos jogos finalizada

--- resources/js/games.js
+++
@@ -1,7 +0,0 @@
-let url = "https://setupme.herokuapp.com/placas_mae";
-
-async function getMotherboards() {
-    const response = await fetch(url)
-    const itens = await response.json();
-    console.log(itens);
-}
--- resources/js/minecraft.js
+++ resources/js/minecraft.js
@@ -1,4 +1,4 @@
-let url = "https://setupme.herokuapp.com/games/fortnite";
+let url = "https://setupme.herokuapp.com/games/minecraft";
 
 async function getMotherboards() {
         const response = await fetch(url)
@@ -13,16 +13,6 @@ async function showMB() {
     document.querySelector("#vm").innerHTML = `<div> Memória da placa de vídeo: ${JSON[0].video_memory} </div>`;
     document.querySelector("#pr").innerHTML = `<div> Processador: ${JSON[0].processador} </div>`;
     document.querySelector("#ram").innerHTML = `<div> Memória RAM: ${JSON[0].ram} </div>`;
-    
 }
 
 showMB();
-
-/*
-async function showMB() {
-    document.querySelector("#table").innerHTML = await getMotherboards();
-}
-
-showMB();
-*/
-
