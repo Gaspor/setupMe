42974972eac1af511f5e91a1819dd11cabde2819
Max <58748971+X86Max@users.noreply.github.com>
2021-10-20 11:08:51 -0300

    Montagem média e avançada - ADICIONADA

---
+++ resources/js/montagem-media.js
@@ -0,0 +1,19 @@
+let url = "https://setupme.herokuapp.com/montagem/media";
+
+async function getMotherboards() {
+        const response = await fetch(url)
+        const itens = await response.json();
+        console.log(itens);
+        return itens;
+}
+
+async function showMB() {
+    const JSON = await getMotherboards();
+    document.querySelector("#pr").innerHTML = `<div> Processador: ${JSON[0].processador} </div>`;
+    document.querySelector("#ram").innerHTML = `<div> Memória RAM: ${JSON[0].ram} </div>`;
+    document.querySelector("#mb").innerHTML = `<div> Placa mãe: ${JSON[0].placa_mae} </div>`;
+    document.querySelector("#stg").innerHTML = `<div> Armazenamento: ${JSON[0].armazenamento} </div>`;
+    document.querySelector("#pv").innerHTML = `<div> Armazenamento: ${JSON[0].placa_video} </div>`;
+}
+
+showMB();
