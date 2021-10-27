1e31ee386de825862b09bbbe6741278f1f254799
Max <58748971+X86Max@users.noreply.github.com>
2021-10-27 14:42:39 -0300

    Update - warning (leia a descrição).
    Se o usuário tentar clicar em montar sem marcar uma opção, aparecerá o aviso (no mobile, por algum motivo desconhecido, ele volta lá para o inicio).

--- resources/js/profissional-estudos.js
+++ resources/js/profissional-estudos.js
@@ -1,8 +1,10 @@
 
+var check = false;
 function checkBasico() {
     let buttons = document.querySelector("#basico");
 
     if(buttons.checked){
+        check = true;
         window.location.href = "montagem-basica.html";
     }
 }
@@ -10,6 +12,7 @@ function checkMedio() {
     let buttons = document.querySelector("#medio");
 
     if(buttons.checked){
+        check = true;
         window.location.href = "montagem-medio.html";
     }
 }
@@ -17,6 +20,14 @@ function checkAvancado() {
     let buttons = document.querySelector("#avançado");
 
     if(buttons.checked){
+        check = true;
         window.location.href = "montagem-avancado.html";
+    } 
+} 
+
+function noCheck(){
+    if(check == false){
+        document.querySelector("#warning").innerHTML = `<div> Marque uma opção! </div>`;
     }
 }
+
