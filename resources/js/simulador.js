7a72def056c035eff7535752846523294ff50c1c
Gaspor <gaspor991@gmail.com>
2021-11-17 16:50:14 -0300

    add: popup (MAX)

--- resources/js/simulador.js
+++ resources/js/simulador.js
@@ -94,3 +94,114 @@ populateDropdown(urlPlaca_mae, "placa_mae");
 populateDropdown(urlRAMS, "ram");
 populateDropdown(urlStorage, "armazenamento");
 populateDropdown(urlFonte, "fonte");
+
+var proces = document.getElementById("processador");
+var placavideo = document.getElementById("placavideo");
+var placamae = document.getElementById("placamae");
+var ram = document.getElementById("ram"); 
+var hd = document.getElementById("hd");
+var ssd = document.getElementById("ssd");
+var fonte = document.getElementById("fonte");
+
+// Variaveis do botão
+var btn_proces = document.getElementById("replace_processador");
+var btn_placa_video = document.getElementById("replace_placa_video");
+var btn_placa_mae = document.getElementById("replace_placa_mae");
+var btn_ram = document.getElementById("replace_ram");
+var btn_hd = document.getElementById("replace_hd");
+var btn_ssd = document.getElementById("replace_ssd");
+var btn_fonte =  document.getElementById("replace_fonte");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks on the button, open the modal
+btn_proces.onclick = function() {
+    proces.style.display = "block";
+}
+
+btn_placa_video.onclick = function(){
+    placavideo.style.display = "block";
+}
+
+btn_placa_mae.onclick = function(){
+    placamae.style.display = "block";
+}
+
+btn_ram.onclick = function(){
+    ram.style.display = "block";
+}
+
+btn_hd.onclick = function(){
+    hd.style.display = "block";
+}
+
+btn_ssd.onclick = function(){
+    ssd.style.display = "block";
+}
+
+btn_fonte.onclick = function(){
+    fonte.style.display = "block";
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    proces.style.display = "none";
+}
+
+span.onclick = function(){
+    placavideo.style.display = "none";
+}
+
+span.onclick = function() {
+    placamae.style.display = "none";
+}
+
+span.onclick = function() {
+    ram.style.display = "none";
+}
+
+span.onclick = function() {
+    hd.style.display = "none";
+}
+
+span.onclick = function() {
+    ssd.style.display = "none";
+}
+
+span.onclick = function() {
+    fonte.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == proces) {
+      proces.style.display = "none";
+    }
+
+    else if(event.target == placavideo){
+        placavideo.style.display = "none";
+    }
+
+    else if(event.target == placamae){
+        placamae.style.display = "none";
+    }
+
+    else if(event.target == ram){
+        ram.style.display = "none";
+    }
+
+    else if(event.target == hd){
+        hd.style.display = "none";
+    }
+
+    else if(event.target == ssd){
+        ssd.style.display = "none";
+    }
+
+    else if(event.target == fonte){
+        fonte.style.display = "none";
+    }
+  }
