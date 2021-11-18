f2b7e680c00f609491e329fb24f91cff9fb20df3
Gaspor <gaspor991@gmail.com>
2021-11-18 02:44:05 -0300

    bugfix: Total value in simulador and href in profissional-estudos

--- resources/js/simulador.js
+++ resources/js/simulador.js
@@ -6,13 +6,34 @@ let urlStorage = "https://setupme.herokuapp.com/armazenamentos";
 let urlFonte = "https://setupme.herokuapp.com/fontes";
 
 let computador = {
-    processador: "",
-    placa_video: "",
-    placa_mae: "",
-    ram: "",
-    hd: "",
-    ssd: "",
-    fonte: "",
+    processador: {
+        nome: "", 
+        valor: ""
+    },
+    placa_video: {
+        nome: "", 
+        valor: ""
+    },
+    placa_mae: {
+        nome: "", 
+        valor: ""
+    },
+    ram: {
+        nome: "", 
+        valor: ""
+    },
+    hd: {
+        nome: "", 
+        valor: ""
+    },
+    ssd: {
+        nome: "", 
+        valor: ""
+    },
+    fonte: {
+        nome: "", 
+        valor: ""
+    },
 }
 
 async function getJson(url) {
@@ -21,9 +42,13 @@ async function getJson(url) {
     return itens;
 }
 
-async function populateDropdown(url, tagName) {
+async function setJson(url, tagName) {
     const JSON = await getJson(url);
 
+    await populateDropdown(JSON, tagName);
+}
+
+async function populateDropdown(JSON, tagName) {
     for (let i = 0; i < JSON.length; i++) {
         const preco = JSON[i].preco;
         const nome = JSON[i].nome;
@@ -43,13 +68,16 @@ async function populateDropdown(url, tagName) {
 }
 
 async function clicked(value, productName, tagName) {
-    await populateComputer(productName, tagName);
+    if (computador[tagName].nome != "") {
+        addInTotal(-computador[tagName].valor);    
+    }
+    await populateComputer(value, productName, tagName);
     changeName(productName, tagName);
     addInTotal(value);
     setNone();
 }
 
-function setNone(){
+function setNone() {
     proces.style.display = "none";
     placavideo.style.display = "none";
     placamae.style.display = "none";
@@ -69,47 +97,46 @@ async function addInTotal(value) {
     precoElement.innerHTML = "R$ " + (preco + parseFloat(value)).toFixed(2);
 }
 
-async function populateComputer(productName, tagName) {
-    computador[tagName] = productName;
-    // console.log(computador[tagName]);
+async function populateComputer(value, productName, tagName) {
+    computador[tagName].nome = productName;
+    computador[tagName].valor = value
 }
 
 function changeP() {
     // console.log(computador.processador);
-    if (computador.processador == "Core i9-10900" && computador.placa_video == "GeForce RTX 3060 Ti XC Gaming" && computador.placa_mae == "B550M DS3H" && computador.ram == "XPG Spectrix D41" && (computador.hd == "HD Barracuda" || computador.ssd == "M2 NVME Blue") && computador.fonte == "Core Reactor") {
+    if (computador.processador.nome == "Core i9-10900" && computador.placa_video.nome == "GeForce RTX 3060 Ti XC Gaming" && computador.placa_mae.nome == "B550M DS3H" && computador.ram.nome == "XPG Spectrix D41" && (computador.hd.nome == "HD Barracuda" || computador.ssd.nome == "M2 NVME Blue") && computador.fonte.nome == "Core Reactor") {
         document.getElementById("probabilidade").innerHTML = "O desempenho desse computador para jogos e para estudos é de aproximadamente 100%.";
         document.getElementById("sistema-operacional").innerHTML = "O sistema operacional recomendado para este computador é Windows 7/8/8.1/10 e Linux.";
         document.getElementById("internet").innerHTML = "O plano de internet recomendado para este computador é de 10 a 50 megas.";
     }
-    if (computador.processador == "Core i7-11700F" && computador.placa_video == "GeForce GTX 1650" && computador.placa_mae == "ROG Strix X570-E Gaming" && computador.ram == "Patriot Viper Elite" && (computador.hd == "HD BarraCuda" || computador.ssd == "M2 NVME SX6000 Lite") && computador.fonte == "GP-P550B") {
+    if (computador.processador.nome == "Core i7-11700F" && computador.placa_video.nome == "GeForce GTX 1650" && computador.placa_mae.nome == "ROG Strix X570-E Gaming" && computador.ram.nome == "Patriot Viper Elite" && (computador.hd.nome == "HD BarraCuda" || computador.ssd.nome == "M2 NVME SX6000 Lite") && computador.fonte.nome == "GP-P550B") {
         document.getElementById("probabilidade").innerHTML = "O desempenho desse computador para jogos e para estudos é de aproximadamente 90%.";
         document.getElementById("sistema-operacional").innerHTML = "O sistema operacional recomendado para este computador é Windows 7/8/8.1/10 e Linux.";
         document.getElementById("internet").innerHTML = "O plano de internet recomendado é de 10 a 50 megas.";
     }
-    if (computador.processador == "Ryzen 5 5600X" && computador.placa_video == "Dual RX 6600 XT O8G" && computador.placa_mae == "ROG MAXIMUS XII FORMULA" && computador.ram == "PNY Performance" && (computador.hd == "HD BarraCuda" || computador.ssd == "M2 NVME SX6000 Lite") && computador.fonte == "GP-P550B") {
+    if (computador.processador.nome == "Ryzen 5 5600X" && computador.placa_video.nome == "Dual RX 6600 XT O8G" && computador.placa_mae.nome == "ROG MAXIMUS XII FORMULA" && computador.ram.nome == "PNY Performance" && (computador.hd.nome == "HD BarraCuda" || computador.ssd.nome == "M2 NVME SX6000 Lite") && computador.fonte.nome == "GP-P550B") {
         document.getElementById("probabilidade").innerHTML = "O desempenho desse computador para jogos e para estudos é de aproximadamente 80%.";
         document.getElementById("sistema-operacional").innerHTML = "O sistema operacional recomendado para este computador é Windows 7/8/8.1/10 e Linux.";
         document.getElementById("internet").innerHTML = "O plano de internet recomendado é de 10 a 50 megas.";
     }
-    if (computador.processador == "Ryzen 5 3600" && computador.placa_video == "Radeon RX 6900XT Limited Black Gaming" && computador.placa_mae == "MPG Z490 GAMING CARBON WIFI" && computador.ram == "PNY Performance" && (computador.hd == "HD BarraCuda" || computador.ssd == "M2 NVME SX6000 Lite") && computador.fonte == "GP-P550B") {
+    if (computador.processador.nome == "Ryzen 5 3600" && computador.placa_video.nome == "Radeon RX 6900XT Limited Black Gaming" && computador.placa_mae.nome == "MPG Z490 GAMING CARBON WIFI" && computador.ram.nome == "PNY Performance" && (computador.hd.nome == "HD BarraCuda" || computador.ssd.nome == "M2 NVME SX6000 Lite") && computador.fonte.nome == "GP-P550B") {
         document.getElementById("probabilidade").innerHTML = "O desempenho desse computador para jogos e para estudos é de aproximadamente 70%.";
         document.getElementById("sistema-operacional").innerHTML = "O sistema operacional recomendado para este computador é Windows 7/8/8.1/10 e Linux.";
         document.getElementById("internet").innerHTML = "O plano de internet recomendado é de 10 a 50 megas.";
     }
 }
 
-
-populateDropdown(urlProcessador, "processador");
-populateDropdown(urlPlaca_video, "placa_video");
-populateDropdown(urlPlaca_mae, "placa_mae");
-populateDropdown(urlRAMS, "ram");
-populateDropdown(urlStorage, "armazenamento");
-populateDropdown(urlFonte, "fonte");
+setJson(urlProcessador, "processador");
+setJson(urlPlaca_video, "placa_video");
+setJson(urlPlaca_mae, "placa_mae");
+setJson(urlRAMS, "ram");
+setJson(urlStorage, "armazenamento");
+setJson(urlFonte, "fonte");
 
 var proces = document.getElementById("processador");
 var placavideo = document.getElementById("placavideo");
 var placamae = document.getElementById("placamae");
-var ram = document.getElementById("ram"); 
+var ram = document.getElementById("ram");
 var hd = document.getElementById("hd");
 var ssd = document.getElementById("ssd");
 var fonte = document.getElementById("fonte");
@@ -121,75 +148,75 @@ var btn_placa_mae = document.getElementById("replace_placa_mae");
 var btn_ram = document.getElementById("replace_ram");
 var btn_hd = document.getElementById("replace_hd");
 var btn_ssd = document.getElementById("replace_ssd");
-var btn_fonte =  document.getElementById("replace_fonte");
+var btn_fonte = document.getElementById("replace_fonte");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
 
 // When the user clicks on the button, open the modal
-btn_proces.onclick = function() {
+btn_proces.onclick = function () {
     proces.style.display = "block";
 }
 
-btn_placa_video.onclick = function(){
+btn_placa_video.onclick = function () {
     placavideo.style.display = "block";
 }
 
-btn_placa_mae.onclick = function(){
+btn_placa_mae.onclick = function () {
     placamae.style.display = "block";
 }
 
-btn_ram.onclick = function(){
+btn_ram.onclick = function () {
     ram.style.display = "block";
 }
 
-btn_hd.onclick = function(){
+btn_hd.onclick = function () {
     hd.style.display = "block";
 }
 
-btn_ssd.onclick = function(){
+btn_ssd.onclick = function () {
     ssd.style.display = "block";
 }
 
-btn_fonte.onclick = function(){
+btn_fonte.onclick = function () {
     fonte.style.display = "block";
 }
 
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     proces.style.display = "none";
 }
 
-span.onclick = function(){
+span.onclick = function () {
     placavideo.style.display = "none";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     placamae.style.display = "none";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     ram.style.display = "none";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     hd.style.display = "none";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     ssd.style.display = "none";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     fonte.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == proces) {
-      proces.style.display = "none";
+        proces.style.display = "none";
     }
 
     else if(event.target == placavideo){
@@ -215,4 +242,4 @@ window.onclick = function(event) {
     else if(event.target == fonte){
         fonte.style.display = "none";
     }
-  }
+}
