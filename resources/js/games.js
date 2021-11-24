0f467494040aa9cd1d58102a6dc980395ddefdf6
Gaspor <gaspor991@gmail.com>
2021-11-24 19:11:33 -0300

    refatoring

--- resources/js/games.js
+++ resources/js/games.js
@@ -1,9 +1,1 @@
-/*const goRight = document.querySelector("#arrow-left");
-const slides = document.querySelectorAll(".slide");
 
-let currentSlide = 0;
-
-goRight.addEventListener("click", () => {
-        goRight.classList.add("active");
-});
-*/
