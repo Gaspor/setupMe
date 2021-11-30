5cacb39a516a7c54e3a892a4de5883ecc952b5bb
Gaspor <gaspor991@gmail.com>
2021-11-30 04:27:42 -0300

    refatoring

--- resources/js/montagem-avancada.js
+++ resources/js/montagem-avancada.js
@@ -3,7 +3,6 @@ let url = "https://setupme.herokuapp.com/montagem/avancada";
 async function getMotherboards() {
         const response = await fetch(url)
         const itens = await response.json();
-        console.log(itens);
         return itens;
 }
 
--- resources/js/montagem-basica.js
+++ resources/js/montagem-basica.js
@@ -3,7 +3,6 @@ let url = "https://setupme.herokuapp.com/montagem/basica";
 async function getMotherboards() {
         const response = await fetch(url)
         const itens = await response.json();
-        console.log(itens);
         return itens;
 }
 
